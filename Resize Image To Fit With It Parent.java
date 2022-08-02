dea6d304013d847a8693ddfd2542b520f4c0b485
TrikayCoder <91646919+TrikayCoder@users.noreply.github.com>
2022-08-02 14:00:04 +0700

    Create Resize Image To Fit With It Parent.java

---
+++ Resize Image To Fit With It Parent.java
@@ -0,0 +1,29 @@
+ public void resizeImage(Bitmap bitmap, ImageView view){
+            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            int width = size.x;
+            int height = bitmap.getHeight() + Math.abs(bitmap.getWidth() - width);
+            view.setImageBitmap(getResizedBitmap(bitmap, width - 20, height - 20));
+        }
+
+        private Bitmap getResizedBitmap(Bitmap image, int maxWidth, int maxHeight) {
+            if (maxHeight > 0 && maxWidth > 0) {
+                int width = image.getWidth();
+                int height = image.getHeight();
+                float ratioBitmap = (float) width / (float) height;
+                float ratioMax = (float) maxWidth / (float) maxHeight;
+                int finalWidth = maxWidth;
+                int finalHeight = maxHeight;
+                if (ratioMax > ratioBitmap) {
+                    finalWidth = (int) ((float)maxHeight * ratioBitmap);
+                } else {
+                    finalHeight = (int) ((float)maxWidth / ratioBitmap);
+                }
+                image = Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true);
+                return image;
+            } else {
+                return image;
+            }
+        }
